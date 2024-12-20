c97909052b8760087f88e0860b8c94ce895a4312
Fiorella <fiorellaroma98@gmail.com>
2024-12-19 16:53:09 -0800

    Archivo de trabajo integrador

---
+++ trabajoPracticoIntegrador.js
@@ -0,0 +1,85 @@
+// 1. Estructura de Datos
+// a) Crear un array de objetos llamado libros que contenga al menos 10
+// libros. Cada libro debe tener las siguientes propiedades: ✓ id (número) ✓ título (string), ✓ autor (string), año (número), género (string),disponible (booleano).
+const libros = [   
+    {
+        id: 1231,
+        titulo: "Harry Potter y la piedra filosofal",
+        autor: "J.K. Rowling",
+        anio: 1997,
+        genero: "Fantasía, Aventura",
+        disponible: true
+    },
+    {
+        id: 1232,
+        titulo: "Harry Potter y la cámara secreta",
+        autor: "J.K. Rowling",
+        anio: 1998,
+        genero: "Fantasía, Aventura",
+        disponible: true
+    },
+    {
+        id: 1233,
+        titulo: "Harry Potter y el prisionero de Azkaban",
+        autor: "J.K. Rowling",
+        anio: 1999,
+        genero: "Fantasía, Aventura",
+        disponible: true
+    },
+    {
+        id: 1234,
+        titulo: "Harry Potter y el cáliz de fuego",
+        autor: "J.K. Rowling",
+        anio: 2000,
+        genero: "Fantasía, Aventura",
+        disponible: true
+    },
+    {
+        id: 1235,
+        titulo: "Harry Potter y la Orden del Fénix",
+        autor: "J.K. Rowling",
+        anio: 2003,
+        genero: "Fantasía, Aventura",
+        disponible: true
+    },
+    {
+        id: 1236,
+        titulo: "Harry Potter y el misterio del príncipe",
+        autor: "J.K. Rowling",
+        anio: 2005,
+        genero: "Fantasía, Aventura",
+        disponible: true
+    },
+    {
+        id: 1237,
+        titulo: "Harry Potter y las reliquias de la muerte",
+        autor: "J.K. Rowling",
+        anio: 2007,
+        genero: "Fantasía, Aventura",
+        disponible: true
+    },
+    {
+        id: 1238,
+        titulo: "El ladrón del rayo",
+        autor: "Rick Riordan",
+        anio: 2005,
+        genero: "Fantasía, Aventura",
+        disponible: true
+    },
+    {
+        id: 1239,
+        titulo: "El mar de los monstruos",
+        autor: "Rick Riordan",
+        anio: 2006,
+        genero: "Fantasía, Aventura",
+        disponible: true
+    },
+    {
+        id: 12310,
+        titulo: "La maldición del titán",
+        autor: "Rick Riordan",
+        anio: 2007,
+        genero: "Fantasía, Aventura",
+        disponible: true
+    }
+];
